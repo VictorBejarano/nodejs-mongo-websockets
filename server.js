68d7d5e5ad7bf040679be63b8a829519f9b1c250
Victor Bejarano <victor.bejarano@sophossolutions.com>
2023-04-11 18:56:24 -0500

    Implementacion del body parser

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
 
 var app = express(); // Inicializa express
 
+app.use(bodyParser.json());
 app.use(router);
 
 // app.use("/", function (req, res) {
 //   res.send("Hola");
 // });
 
+router.get("/", function (req, res) {
+  res.send("Hola desde GET");
+});
 
+router.post("/", function (req, res) {
+  console.log(req.query);
+  console.log(req.body);
 
-router.get('/', function (req, res) {
-    res.send("Hola desde GET");
-})
-
-router.post('/', function (req, res) {
-    res.send("Hola desde POST");
-})
+  res.send(`Hola desde POST, mensaje añadido correctamente ${req.query.message}`);
+});
 
 app.listen(3000);
-console.log("La aplicacion esta escuchando en http://localhost:3000");
+console.log(
+  `La aplicacion esta escuchando en http://localhost:3000`
+);
