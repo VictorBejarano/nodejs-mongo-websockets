fb7e68b2c6f40358b6fd13a7d705b20f42cdcc37
Victor Bejarano <victor.bejarano@sophossolutions.com>
2023-04-11 18:47:59 -0500

    Metodos usando Router de express

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const router = express.Router();
 
 var app = express(); // Inicializa express
 
-app.use("/", function (req, res) {
-  res.send("Hola");
-});
+app.use(router);
+
+// app.use("/", function (req, res) {
+//   res.send("Hola");
+// });
+
+
+
+router.get('/', function (req, res) {
+    res.send("Hola desde GET");
+})
+
+router.post('/', function (req, res) {
+    res.send("Hola desde POST");
+})
 
 app.listen(3000);
 console.log("La aplicacion esta escuchando en http://localhost:3000");
